442f1d9d03f8ef42f05e43f94627dcf8beb90a48
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-12-28 03:48:45 +0100

    It's rotating...

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // "use strict";
 
-var GROW_DELAY_MS = 50;
 var GROWTH = 2;
 var DEFAULT_WIDTH_UNIT = "px";
+var GROWTH_DELAY_MS = 50;
+var BOB_DELAY_MS = 50;
+var BOB_STRENGTH = 2;
 
 var meatballMan;
-var meatballManGrowInterval;
+var growthInterval;
+var bobInterval;
 
 function itsComingCloser() {
     var currentWidth;
@@ -13,7 +16,7 @@ function itsComingCloser() {
 
     var widthMatch = meatballMan.style.width.match(/^(\d+)(.+)$/);
 
-    if (widthMatch) {
+    if (widthMatch && widthMatch[1] && widthMatch[2]) {
         currentWidth = Number(widthMatch[1]);
         widthUnit = widthMatch[2];
     } else {
@@ -26,10 +29,30 @@ function itsComingCloser() {
     }
 }
 
+function itsBobbing() {
+    var halfBobStrength = BOB_STRENGTH * 0.5;
+    var currentRotation;
+
+    var rotationMatch = meatballMan.style.transform.match(/^rotate\((\d+)deg\)$/);
+
+    if (rotationMatch && rotationMatch[1]) {
+        currentRotation = Number(rotationMatch[1]);
+    } else {
+        currentRotation = 0;
+    }
+
+    var rotRand = Math.random() * BOB_STRENGTH - currentRotation;
+    var rotMult = rotRand >= halfBobStrength ? 1.0 : -1.0;
+    meatballMan.style.transform = "rotate("
+        + String(currentRotation + BOB_STRENGTH * rotMult)
+        + "deg)";
+}
+
 function main() {
     meatballMan = document.getElementById("man");
     if (meatballMan) {
-        meatballManGrowInterval = setInterval(itsComingCloser, GROW_DELAY_MS);
+        growthInterval = setInterval(itsComingCloser, GROWTH_DELAY_MS);
+        bobInterval = setInterval(itsBobbing, BOB_DELAY_MS);
     }
 }
 
