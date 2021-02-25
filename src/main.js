4d1b8470fe512907e9584674c3f999fb7492ee50
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2021-02-25 20:22:35 +0100

    Add egghead variant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,33 @@
 
 var context = null;
 
+function main() {
+    var variant = queryParams().variant;
+    switch (variant) {
+        case "meatball-man":
+        case "meatball":
+        case "man":
+            document.querySelector("img#man").src = "./public/man.png";
+            break;
+        case "egghead":
+            document.querySelector("img#man").src = "./public/egghead.png";
+            break;
+    }
+    letItConsume();
+}
+
+function queryParams() {
+    var params = {};
+    window.location.search
+        .replace(/^\?/, "")
+        .split("&")
+        .map((keyVal) => {
+            var split = keyVal.split("=");
+            params[split[0]] = split[1];
+        });
+    return params;
+}
+
 function letItConsume() {
     context = getContext();
     initMeatballMan();
@@ -23,4 +50,4 @@ function printError(errMsg) {
     }
 }
 
-window.onload = letItConsume;
+window.onload = main;
